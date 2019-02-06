2a1e8f741e7e512379e121301b414bfe79bb0906
BeckieQ <beckie.quinlan@gmail.com>
2019-02-06 01:18:31 +0000

    Add translations (es)

--- app/src/main/java/com/beckiebrown/justjava/MainActivity.java
+++ app/src/main/java/com/beckiebrown/justjava/MainActivity.java
@@ -1,9 +1,16 @@
 package com.beckiebrown.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -22,14 +29,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked
      */
     public void increment(View view) {
+        if (quantity >= 100) {
+            Toast.makeText(this, "You cannot order more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
+
     }
 
     /**
      * This method is called when the minus button is clicked
      */
     public void decrement(View view) {
+        if (quantity <= 1) {
+            Toast.makeText(this, "You cannot order less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
@@ -38,29 +54,65 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayMessage(callOrderSummary());
+
+        EditText userNameField = findViewById(R.id.user_name);
+        String userName = userNameField.getText().toString();
+
+        CheckBox whippedCream = findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCream.isChecked();
+
+        CheckBox chocolate = findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolate.isChecked();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); //only email apps should handle this intent
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.order_summary_email_subject));
+        intent.putExtra(Intent.EXTRA_TEXT, callOrderSummary(userName, price, hasWhippedCream, hasChocolate));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 
     /**
      * This method creates the order summary message
      *
-     * @return order summary message
+     * @param name            of customer
+     * @param price           of order
+     * @param addWhippedCream Status of whipped cream checkbox
+     * @param addChocolate    Status of chocolate checkbox
+     * @return order summary text message
      */
-    private String callOrderSummary() {
-        String priceMessage = "Name: Beckie the Boss";
-        priceMessage += "\nQuantity: " + quantity;
-        priceMessage += "\nTotal: £" + calculatePrice();
-        priceMessage += "\nThank You!";
+    private String callOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage = getString(R.string.order_summary_name, name);
+        priceMessage += "\n" + getString(R.string.order_summary_whipped_cream, addWhippedCream);
+        priceMessage += "\n" + getString(R.string.order_summary_chocolate, addChocolate);
+        priceMessage += "\n" + getString(R.string.order_summary_quantity, quantity);
+        priceMessage += "\n" + getString(R.string.order_summary_price, price);
+        priceMessage += "\n" + getString(R.string.thank_you);
         return priceMessage;
     }
 
     /**
      * Calculates the price of the order.
      *
-     * @return total price
+     * @param hasWhippedCream Status of whipped cream checkbox
+     * @param hasChocolate    Status of chocolate checkbox
+     * @return totalPrice
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+        int price = 5;
+
+        if (hasWhippedCream) {
+            price += 1;
+        }
+        if (hasChocolate) {
+            price += 2;
+        }
+        int totalPrice = quantity * price;
+        return totalPrice;
     }
 
     /**
